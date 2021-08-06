3f16510d86d0124151206ed5d122d135bb0d57cc
jamallangoy <81915438+jamallangoy@users.noreply.github.com>
2021-08-05 20:16:33 -0400

    Add files via upload

---
+++ fetch.jsx
@@ -0,0 +1,151 @@
+const Pagination = ({ items, pageSize, onPageChange }) => {
+    if (items.length <= 1) return null;
+  
+    let num = Math.ceil(items.length / pageSize);
+    let pages = range(1, num + 1);
+    const list = pages.map(page => {
+      return (
+        <button key={page} onClick={onPageChange} className="page-item">
+          {page}
+        </button>
+      );
+    });
+    return (
+      <nav>
+        <ul className="pagination">{list}</ul>
+      </nav>
+    );
+  };
+  const range = (start, end) => {
+    return Array(end - start + 1)
+      .fill(0)
+      .map((item, i) => start + i);
+  };
+  function paginate(items, pageNumber, pageSize) {
+    const start = (pageNumber - 1) * pageSize;
+    let page = items.slice(start, start + pageSize);
+    return page;
+  }
+  const useDataApi = (initialUrl, initialData) => {
+    const { useState, useEffect, useReducer } = React;
+    const [url, setUrl] = useState(initialUrl);
+  
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+      isLoading: false,
+      isError: false,
+      data: initialData
+    });
+  
+    useEffect(() => {
+      let didCancel = false;
+      const fetchData = async () => {
+        dispatch({ type: "FETCH_INIT" });
+        try {
+          const result = await axios(url);
+          if (!didCancel) {
+            dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+          }
+        } catch (error) {
+          if (!didCancel) {
+            dispatch({ type: "FETCH_FAILURE" });
+          }
+        }
+      };
+      fetchData();
+      return () => {
+        didCancel = true;
+      };
+    }, [url]);
+    return [state, setUrl];
+  };
+  const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_INIT":
+        return {
+          ...state,
+          isLoading: true,
+          isError: false
+        };
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload
+        };
+      case "FETCH_FAILURE":
+        return {
+          ...state,
+          isLoading: false,
+          isError: true
+        };
+      default:
+        throw new Error();
+    }
+  };
+  // App that gets data from Hacker News url
+  function App() {
+    const { Fragment, useState, useEffect, useReducer } = React;
+    const [query, setQuery] = useState("MIT");
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 5;
+    const [{ data, isLoading, isError }, doFetch] = useDataApi(
+      "https://hn.algolia.com/api/v1/search?query=MIT",
+      {
+        hits: []
+      }
+    );
+    const handlePageChange = e => {
+      setCurrentPage(Number(e.target.textContent));
+    };
+    let page = data.hits;
+    if (page.length >= 1) {
+      page = paginate(page, currentPage, pageSize);
+      console.log(`currentPage: ${currentPage}`);
+    }
+    return (
+      <Fragment>
+
+        <h2 className='header'> Geddit</h2>
+        <form
+          onSubmit={event => {
+            doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
+            event.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)}
+            className='input'
+          />
+          <button type="submit" className='button'>Search</button>
+        </form>
+
+        <Pagination
+          items={data.hits}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+        ></Pagination>
+  
+        {isError && <div>Something went wrong ...</div>}
+  
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          <ol className='side'>
+            {page.map(item => (
+              <li key={item.objectID} className='content'>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ol>
+        )}
+        
+      </Fragment>
+    );
+  }
+  
+  // ========================================
+  ReactDOM.render(<App />, document.getElementById("root"));
+  
